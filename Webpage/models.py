811caaa658979a1bedfce4ad0a036b9b67ba8412
MegaBeedrillTAO <jakekremling@yahoo.com>
2020-02-05 22:18:46 -0600

    Got the database info to be rendered on page. Just need to figure out adding css to better style and format it.

--- Webpage/models.py
+++ Webpage/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 
 class Card(models.Model):
-    title = models.CharField(max_length=20)
-    term_length = models.CharField(max_length=15)
+    titles = [('Power of Credit', 'Power of Credit'), ('New Fixed', 'New Fixed')]
+    title = models.CharField(max_length=15, choices=titles)
+    term_length = models.IntegerField()
     description = models.TextField()
     rate = models.FloatField()
